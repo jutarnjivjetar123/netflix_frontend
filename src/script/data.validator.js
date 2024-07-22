172a50e614e397875f6e8f956a3cffd5c5faf9c4
MaKe <mahirkeran69@gmail.com>
2024-07-22 15:04:59 +0200

    Updated login.html, login.css, and data.validator.js to include password validation, turned off autocomplete, until further notice, it is not neccessary to turn it on, because autofill method must be understood, and implemented to fix it

--- src/script/data.validator.js
+++ src/script/data.validator.js
@@ -58,15 +58,47 @@ function validatePhoneNumberOrEmailUserInput() {
 window.validatePhoneNumberOrEmailUserInput =
   validatePhoneNumberOrEmailUserInput;
 
+function validatePassword() {
+  const passwordElement = document.querySelector("#passwordInput");
+  const passwordValue = document.querySelector("#passwordInput").value;
+  const passwordWarningElement = document.querySelector(".password-warning");
+  if (passwordWarningElement.childElementCount > 0) {
+    while (passwordWarningElement.firstChild) {
+      passwordWarningElement.removeChild(passwordWarningElement.firstChild);
+    }
+  }
+
+  const span = document.createElement("span");
+  const label = document.createElement("label");
+  span.classList = "material-symbols-outlined";
+  span.style.fontSize = "16px";
+  passwordWarningElement.appendChild(span);
+  passwordWarningElement.appendChild(label);
+
+  if (passwordValue.trim().length < 1) {
+    console.log("Password string is empty");
+    span.textContent = "info";
+    label.textContent = "Password is required";
+    return;
+  }
+  console.log("Password is OK");
+  span.textContent = "";
+  label.textContent = "";
+}
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".signInButton").addEventListener("click", () => {
     validatePhoneNumberOrEmailUserInput();
+    validatePassword();
     document
       .querySelector("#emailOrPhoneInput")
       .addEventListener("input", () => {
         console.log("Validation active");
         validatePhoneNumberOrEmailUserInput();
       });
+    document.querySelector("#passwordInput").addEventListener("input", () => {
+      console.log("Password validation active");
+      validatePassword();
+    });
   });
 });
 
